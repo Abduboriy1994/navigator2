afba49deb70908ec611e875baf1af51581d41957
Abduboriy1994 <abduboriymurodilov@gmail.com>
2021-10-22 14:33:16 +0500

    Home ni asosiy ga ozgardi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Asosiy Page"),
         centerTitle: true,
       ),
       body: Center(
